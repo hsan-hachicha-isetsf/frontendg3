3d740f51e26bb9e75c1206790712748fa4fe8065
hsan-hachicha-isetsf <hsan.hachicha@gmail.com>
2024-10-31 10:49:07 +0100

    Commit new version

--- src/components/categories/Insertcategorie.jsx
+++ src/components/categories/Insertcategorie.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form } from 'react-bootstrap'
 
 const Insertcategorie = () => {
+  const[categorie,setCategorie]=useState({})
   return (
     <div>
-      Insérer une catégorie
+      <Form>
+      <Form.Group className="mb-3">
+        <Form.Label>Nom catégorie</Form.Label>
+        <Form.Control 
+        type="text" 
+        required
+        placeholder="Nom catégorie" 
+        value={categorie.nomcategorie}
+        onChange={(e)=>setCategorie({...categorie,nomcategorie:e.target.value})}
+        
+        />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Image catégorie</Form.Label>
+        <Form.Control 
+        type="text" 
+        required
+        placeholder="Image catégorie" 
+        value={categorie.imagecategorie}
+        onChange={(e)=>setCategorie({...categorie,imagecategorie:e.target.value})}
+        
+        />
+      </Form.Group>
+        <div>
+          <button className='btn btn-success btn-sm'> <i class="fa-solid fa-floppy-disk"></i>Enregistrer</button>
+          &nbsp;
+          <button className='btn btn-danger btn-sm'><i class="fa-solid fa-circle-xmark"></i> Annuler</button>
+        </div>
+    </Form>
     </div>
   )
 }
--- src/components/categories/Listcategories.jsx
+++ src/components/categories/Listcategories.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
-
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import ReactLoading from 'react-loading';
+import { Link } from 'react-router-dom';
 const Listcategories = () => {
+    const[categories,setCategories]=useState([])
+    const[isLoading,setIsLoading]=useState(true)
+
+    const fetchcategories=async()=>{
+        try {
+            const res=await axios.get("https://backendecomgs1.vercel.app/api/api/categories")
+           
+            setCategories(res.data)
+            setIsLoading(false)
+          
+
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+useEffect(()=>{
+    fetchcategories()
+},[])
+
+if(isLoading){
+    return <center><ReactLoading type="spinningBubbles" color="red" height={'20%'} width={'20%'} /></center>
+}
+const handleDelete=async(id)=>{
+    if(window.confirm("Êtes vous sûre de vouloir supprimer")){
+    try {
+        await axios.delete(`https://backendecomgs1.vercel.app/api/api/categories/${id}`)
+        .then(res=>{  
+            setCategories(categories.filter(c=>c.id!=id))
+        })
+
+    } catch (error) {
+       console.log(error) 
+    }
+}
+}
   return (
     <div>
-      Liste des catégories
+       <Link to="/categories/add"> <button className='btn btn-success btn-sm'><i class="fa-solid fa-square-plus"></i> Nouveau</button></Link>
+      <center><h1>Liste des catégories</h1></center>
+      <table className='table table-striped'>
+        <thead>
+            <tr>
+                <th>Nom Catégorie</th>
+                <th>Image Catégorie</th>
+                <th>Update</th>
+                <th>Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+           { categories.map((cat,index)=>
+            <tr key={index}>
+                <td>{cat.nomcategorie}</td>
+                <td><img src={cat.imagecategorie} width={100} height={100}/></td>
+                <td><button className='btn btn-warning btn-sm'><i class="fa-solid fa-pen-to-square"></i> Update</button></td>
+                <td><button className='btn btn-danger btn-sm' onClick={()=>handleDelete(cat.id)}><i class="fa-solid fa-trash"></i> Delete</button></td>
+            </tr>
+
+            )}
+
+        </tbody>
+      </table>
     </div>
   )
 }
